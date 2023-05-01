0891c221248d749e16185c785b6ab68173b4afe6
kavyak21 <119100292+kavyak21@users.noreply.github.com>
2023-05-01 15:55:20 +0530

    NB_Predict.py

--- NB_Predict.py
+++ NB_Predict.py
@@ -1,40 +1,34 @@
 import pandas as pd
-import streamlit as st
-
-# Read our dataset using read_csv()
-bbc_text = pd.read_csv(r'C:\Users\DELL\Desktop\NB_predictor\bbc-text.txt')
-bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
-bbc_text.head()
-
-bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
-bbc_text.category.unique()
-
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import streamlit as st
+import pickle
+bbc_text = pd.read_csv(r"D:\STUDY\Sem2\NLP\NLPIR\bbc-text.txt")
+bbc_text = bbc_text.rename(columns={'text': 'News_Headline'}, inplace=False)
+bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
+
 X = bbc_text.News_Headline
 y = bbc_text.category
-#split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=1)
 
-from sklearn.feature_extraction.text import CountVectorizer
-vector = CountVectorizer(stop_words = 'english',lowercase=False)
-# fit the vectorizer on the training data
+vector = CountVectorizer(stop_words='english', lowercase=False)
 vector.fit(X_train)
-vector.vocabulary_
 X_transformed = vector.transform(X_train)
-X_transformed.toarray()
-# for test data
 X_test_transformed = vector.transform(X_test)
 
-from sklearn.naive_bayes import MultinomialNB
 naivebayes = MultinomialNB()
 naivebayes.fit(X_transformed, y_train)
 
-input = st.text_area("Enter the text")
-if st.button("Category"):
-   vec = vector.transform(input).toarray()
-   predict = list(naivebayes.predict(vec))[0].replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS')
-   st.write(predict.text)
+with open('model.pkl', 'wb') as f:
+    pickle.dump(naivebayes, f)
 
-
-
+st.header('Demo')
+input_text = st.text_area("Please enter the text", value="")
+if st.button("Check"):
+    input_text_transformed = vector.transform([input_text]).toarray()
+    prediction = naivebayes.predict(input_text_transformed)[0]
+    prediction_mapping = {0: 'TECH', 1: 'BUSINESS', 2: 'SPORTS', 3: 'ENTERTAINMENT', 4: 'POLITICS'}
+    result = prediction_mapping[prediction]
+    st.write(f"Predicted category: {result}")
 
